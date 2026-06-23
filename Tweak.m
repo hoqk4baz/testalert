83f6a3248df890c819deede930562a4c252acc94
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2026-06-24 00:13:20 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -2,7 +2,6 @@
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
 #import <Security/Security.h>
-#import <dlfcn.h>
 #include "fishhook.h"
 
 static NSUUID *fakeIDFV = nil;
@@ -13,61 +12,73 @@ static NSUUID *hook_identifierForVendor(id self, SEL _cmd) {
     return fakeIDFV;
 }
 
-// SecItemCopyMatching hook
-typedef OSStatus (*SecItemCopyMatching_t)(CFDictionaryRef query, CFTypeRef *result);
-static SecItemCopyMatching_t original_SecItemCopyMatching = NULL;
+// SecItemCopyMatching hook - okuma
+typedef OSStatus (*SecItemCopyMatching_t)(CFDictionaryRef, CFTypeRef *);
+static SecItemCopyMatching_t orig_CopyMatching = NULL;
 
-static OSStatus hook_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *result) {
-    OSStatus status = original_SecItemCopyMatching(query, result);
+// SecItemAdd hook - ilk yazma
+typedef OSStatus (*SecItemAdd_t)(CFDictionaryRef, CFTypeRef *);
+static SecItemAdd_t orig_Add = NULL;
+
+// SecItemUpdate hook - güncelleme
+typedef OSStatus (*SecItemUpdate_t)(CFDictionaryRef, CFDictionaryRef);
+static SecItemUpdate_t orig_Update = NULL;
+
+static NSData *fakeData() {
+    return [fakeRawValue dataUsingEncoding:NSUTF8StringEncoding];
+}
+
+static void replaceDictResult(CFTypeRef *result) {
+    if (!result || !*result) return;
     
-    if (status != errSecSuccess || !result || !*result) return status;
-    
-    NSData *fakeData = [fakeRawValue dataUsingEncoding:NSUTF8StringEncoding];
-    
-    // Tek item - NSDictionary
     if (CFGetTypeID(*result) == CFDictionaryGetTypeID()) {
         NSDictionary *item = (__bridge NSDictionary *)*result;
-        NSData *valueData = item[(__bridge id)kSecValueData];
-        if (valueData) {
-            NSString *original = [[NSString alloc] initWithData:valueData encoding:NSUTF8StringEncoding];
-            NSLog(@"[Spoofer] Keychain orijinal: %@", original);
-            
-            NSMutableDictionary *mutable = [item mutableCopy];
-            mutable[(__bridge id)kSecValueData] = fakeData;
-            CFTypeRef new = (__bridge_retained CFTypeRef)mutable;
+        if (item[(__bridge id)kSecValueData]) {
+            NSMutableDictionary *m = [item mutableCopy];
+            m[(__bridge id)kSecValueData] = fakeData();
+            CFTypeRef n = (__bridge_retained CFTypeRef)m;
             CFRelease(*result);
-            *result = new;
+            *result = n;
         }
-        return status;
-    }
-    
-    // Çoklu item - NSArray
-    if (CFGetTypeID(*result) == CFArrayGetTypeID()) {
+    } else if (CFGetTypeID(*result) == CFArrayGetTypeID()) {
         NSArray *items = (__bridge NSArray *)*result;
-        NSMutableArray *mutable = [NSMutableArray arrayWithCapacity:items.count];
-        
-        for (id item in items) {
-            if ([item isKindOfClass:[NSDictionary class]]) {
-                NSData *valueData = item[(__bridge id)kSecValueData];
-                if (valueData) {
-                    NSString *original = [[NSString alloc] initWithData:valueData encoding:NSUTF8StringEncoding];
-                    NSLog(@"[Spoofer] Keychain orijinal: %@", original);
-                    
-                    NSMutableDictionary *mutableItem = [item mutableCopy];
-                    mutableItem[(__bridge id)kSecValueData] = fakeData;
-                    [mutable addObject:mutableItem];
-                    continue;
-                }
+        NSMutableArray *m = [NSMutableArray arrayWithCapacity:items.count];
+        for (NSDictionary *item in items) {
+            if ([item isKindOfClass:[NSDictionary class]] && item[(__bridge id)kSecValueData]) {
+                NSMutableDictionary *mi = [item mutableCopy];
+                mi[(__bridge id)kSecValueData] = fakeData();
+                [m addObject:mi];
+            } else {
+                [m addObject:item];
             }
-            [mutable addObject:item];
         }
-        
-        CFTypeRef new = (__bridge_retained CFTypeRef)mutable;
+        CFTypeRef n = (__bridge_retained CFTypeRef)m;
         CFRelease(*result);
-        *result = new;
+        *result = n;
     }
-    
+}
+
+static OSStatus hook_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *result) {
+    OSStatus status = orig_CopyMatching(query, result);
+    if (status == errSecSuccess) replaceDictResult(result);
     return status;
+}
+
+static OSStatus hook_SecItemAdd(CFDictionaryRef attributes, CFTypeRef *result) {
+    // Yazılan değeri fake ile değiştir
+    NSMutableDictionary *m = [(__bridge NSDictionary *)attributes mutableCopy];
+    if (m[(__bridge id)kSecValueData]) {
+        m[(__bridge id)kSecValueData] = fakeData();
+    }
+    return orig_Add((__bridge CFDictionaryRef)m, result);
+}
+
+static OSStatus hook_SecItemUpdate(CFDictionaryRef query, CFDictionaryRef attributesToUpdate) {
+    NSMutableDictionary *m = [(__bridge NSDictionary *)attributesToUpdate mutableCopy];
+    if (m[(__bridge id)kSecValueData]) {
+        m[(__bridge id)kSecValueData] = fakeData();
+    }
+    return orig_Update(query, (__bridge CFDictionaryRef)m);
 }
 
 @interface DeviceSpoofer : NSObject
@@ -76,29 +87,26 @@ static OSStatus hook_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *resul
 @implementation DeviceSpoofer
 
 + (void)load {
-    // Her açılışta yeni random değer üret
     fakeIDFV = [NSUUID UUID];
     fakeRawValue = [fakeIDFV UUIDString];
     
+    NSLog(@"[Spoofer] Fake ID: %@", fakeRawValue);
+    
     // IDFV hook
-    Method m = class_getInstanceMethod(
-        objc_getClass("UIDevice"),
-        @selector(identifierForVendor)
-    );
+    Method m = class_getInstanceMethod(objc_getClass("UIDevice"), @selector(identifierForVendor));
     if (m) method_setImplementation(m, (IMP)hook_identifierForVendor);
     
-    // SecItemCopyMatching hook - fishhook ile
-    rebind_symbols((struct rebinding[1]){
-        {"SecItemCopyMatching", hook_SecItemCopyMatching, (void **)&original_SecItemCopyMatching}
-    }, 1);
-    
-    NSLog(@"[Spoofer] Yüklendi. Fake IDFV: %@", fakeRawValue);
+    // Keychain hook - okuma + yazma
+    rebind_symbols((struct rebinding[3]){
+        {"SecItemCopyMatching", hook_SecItemCopyMatching, (void **)&orig_CopyMatching},
+        {"SecItemAdd",          hook_SecItemAdd,          (void **)&orig_Add},
+        {"SecItemUpdate",       hook_SecItemUpdate,       (void **)&orig_Update},
+    }, 3);
     
     // Alert
     dispatch_async(dispatch_get_main_queue(), ^{
         dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(1.5 * NSEC_PER_SEC)),
                        dispatch_get_main_queue(), ^{
-            
             UIWindow *window = nil;
             for (UIScene *scene in [UIApplication sharedApplication].connectedScenes) {
                 if ([scene isKindOfClass:[UIWindowScene class]]) {
@@ -110,14 +118,10 @@ static OSStatus hook_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *resul
             
             UIAlertController *alert = [UIAlertController
                 alertControllerWithTitle:@"✅ Device ID Değiştirildi"
-                message:[NSString stringWithFormat:@"Fake IDFV:\n%@", fakeRawValue]
+                message:[NSString stringWithFormat:@"Bu oturumun ID'si:\n%@", fakeRawValue]
                 preferredStyle:UIAlertControllerStyleAlert
             ];
-            [alert addAction:[UIAlertAction
-                actionWithTitle:@"Tamam"
-                style:UIAlertActionStyleDefault
-                handler:nil
-            ]];
+            [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
             [window.rootViewController presentViewController:alert animated:YES completion:nil];
         });
     });

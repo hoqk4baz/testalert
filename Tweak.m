2f7010f6e84b94d9e2d85698c8bc7e0f21ee61e5
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2026-06-24 01:16:01 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -6,69 +6,47 @@
 
 static NSMutableArray *logs = nil;
 
-static void addLog(NSString *source, NSString *value) {
-    if (!value || value.length < 8) return;
+static void addLog(NSString *msg) {
     @synchronized(logs) {
-        [logs addObject:[NSString stringWithFormat:@"[%@] %@", source, value]];
+        [logs addObject:msg];
     }
 }
 
-// IDFV
+// 1. IDFV
 static NSUUID *hook_identifierForVendor(id self, SEL _cmd) {
+    addLog(@"IDFV çağrıldı");
     IMP orig = class_getMethodImplementation(objc_getClass("UIDevice"), @selector(identifierForVendor));
-    NSUUID *result = ((NSUUID*(*)(id,SEL))orig)(self, _cmd);
-    addLog(@"IDFV", result.UUIDString);
-    return result;
+    return ((NSUUID*(*)(id,SEL))orig)(self, _cmd);
 }
 
-// Keychain
+// 2. Keychain
 typedef OSStatus (*SecItemCopyMatching_t)(CFDictionaryRef, CFTypeRef*);
 static SecItemCopyMatching_t orig_SecItemCopyMatching = NULL;
-
 static OSStatus hook_SecItemCopyMatching(CFDictionaryRef query, CFTypeRef *result) {
-    OSStatus status = orig_SecItemCopyMatching(query, result);
     @try {
-        if (status == errSecSuccess && result && *result) {
-            NSDictionary *q = (__bridge NSDictionary*)query;
-            NSString *service = q[(__bridge id)kSecAttrService] ?: @"?";
-            if (CFGetTypeID(*result) == CFDictionaryGetTypeID()) {
-                NSData *data = ((__bridge NSDictionary*)*result)[(__bridge id)kSecValueData];
-                if (data) {
-                    NSString *val = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                    addLog([NSString stringWithFormat:@"Keychain[%@]", service], val);
-                }
-            } else if (CFGetTypeID(*result) == CFArrayGetTypeID()) {
-                for (NSDictionary *item in (__bridge NSArray*)*result) {
-                    NSData *data = item[(__bridge id)kSecValueData];
-                    NSString *s = item[(__bridge id)kSecAttrService] ?: service;
-                    if (data) {
-                        NSString *val = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
-                        addLog([NSString stringWithFormat:@"Keychain[%@]", s], val);
-                    }
-                }
-            }
-        }
+        NSDictionary *q = (__bridge NSDictionary*)query;
+        NSString *service = q[(__bridge id)kSecAttrService] ?: @"?";
+        addLog([NSString stringWithFormat:@"Keychain okundu: %@", service]);
     } @catch(NSException *e) {}
-    return status;
+    return orig_SecItemCopyMatching(query, result);
 }
 
-// NSFileManager - crash yapmaz çünkü path filtresi var
+// 3. NSFileManager
 static NSData *(*orig_contentsAtPath)(id, SEL, NSString*) = NULL;
 static NSData *hook_contentsAtPath(id self, SEL _cmd, NSString *path) {
-    NSData *result = orig_contentsAtPath(self, _cmd, path);
     @try {
-        if (result && path) {
-            NSString *lower = path.lowercaseString;
-            if ([lower containsString:@"uuid"] ||
-                [lower containsString:@"device"] ||
-                [lower containsString:@"udid"] ||
-                [lower containsString:@"ident"]) {
-                NSString *val = [[NSString alloc] initWithData:result encoding:NSUTF8StringEncoding];
-                if (val) addLog([NSString stringWithFormat:@"File[%@]", path.lastPathComponent], val);
-            }
-        }
+        addLog([NSString stringWithFormat:@"File okundu: %@", path.lastPathComponent]);
     } @catch(NSException *e) {}
-    return result;
+    return orig_contentsAtPath(self, _cmd, path);
+}
+
+// 4. NSUserDefaults
+static id (*orig_objectForKey)(id, SEL, NSString*) = NULL;
+static id hook_objectForKey(id self, SEL _cmd, NSString *key) {
+    @try {
+        addLog([NSString stringWithFormat:@"UserDefaults: %@", key]);
+    } @catch(NSException *e) {}
+    return orig_objectForKey(self, _cmd, key);
 }
 
 @interface DeviceLogger : NSObject
@@ -79,17 +57,27 @@ static NSData *hook_contentsAtPath(id self, SEL _cmd, NSString *path) {
 + (void)load {
     logs = [NSMutableArray array];
     
+    // IDFV
     Method m1 = class_getInstanceMethod(objc_getClass("UIDevice"), @selector(identifierForVendor));
     if (m1) method_setImplementation(m1, (IMP)hook_identifierForVendor);
     
+    // Keychain
     rebind_symbols((struct rebinding[1]){
         {"SecItemCopyMatching", hook_SecItemCopyMatching, (void**)&orig_SecItemCopyMatching}
     }, 1);
     
-    Method m4 = class_getInstanceMethod(objc_getClass("NSFileManager"), @selector(contentsAtPath:));
+    // NSFileManager
+    Method m3 = class_getInstanceMethod(objc_getClass("NSFileManager"), @selector(contentsAtPath:));
+    if (m3) {
+        orig_contentsAtPath = (NSData*(*)(id,SEL,NSString*))method_getImplementation(m3);
+        method_setImplementation(m3, (IMP)hook_contentsAtPath);
+    }
+    
+    // NSUserDefaults
+    Method m4 = class_getInstanceMethod(objc_getClass("NSUserDefaults"), @selector(objectForKey:));
     if (m4) {
-        orig_contentsAtPath = (NSData*(*)(id,SEL,NSString*))method_getImplementation(m4);
-        method_setImplementation(m4, (IMP)hook_contentsAtPath);
+        orig_objectForKey = (id(*)(id,SEL,NSString*))method_getImplementation(m4);
+        method_setImplementation(m4, (IMP)hook_objectForKey);
     }
     
     dispatch_async(dispatch_get_main_queue(), ^{
@@ -105,15 +93,17 @@ static NSData *hook_contentsAtPath(id self, SEL _cmd, NSString *path) {
             }
             if (!window) return;
             
-            NSString *combined = logs.count > 0
-                ? [logs componentsJoinedByString:@"\n"]
+            // Sadece unique loglar
+            NSOrderedSet *unique = [NSOrderedSet orderedSetWithArray:logs];
+            NSString *combined = unique.count > 0
+                ? [[unique array] componentsJoinedByString:@"\n"]
                 : @"Tespit edilemedi";
             
             [UIPasteboard generalPasteboard].string = combined;
             
             UIAlertController *alert = [UIAlertController
-                alertControllerWithTitle:@"Device ID Kaynakları"
-                message:[NSString stringWithFormat:@"%lu kaynak\nPanoya kopyalandı", (unsigned long)logs.count]
+                alertControllerWithTitle:@"Hook Tetiklenenler"
+                message:[NSString stringWithFormat:@"%lu unique\nPanoya kopyalandı", (unsigned long)unique.count]
                 preferredStyle:UIAlertControllerStyleAlert
             ];
             [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];

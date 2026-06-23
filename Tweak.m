3c6988ed54ead5f46d225c9d4209ae9136faa414
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2026-06-24 00:29:48 +0300

    Update Tweak.m

--- Tweak.m
+++ Tweak.m
@@ -1,46 +1,25 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 #import <objc/runtime.h>
-#import <Security/Security.h>
-#include "fishhook.h"
 
 static NSMutableArray *foundLogs = nil;
-static NSString *targetPrefix = @"77bc647f"; // ilk 8 char yeterli
+static NSString *targetValue = @"77bc647f";
 
-// NSString isEqualToString hook - bu değerin nerede karşılaştırıldığını bul
-typedef BOOL (*isEqual_t)(id, SEL, id);
-static isEqual_t orig_isEqual = NULL;
+static void (*orig_setValue)(id, SEL, NSString*, NSString*) = NULL;
 
-static BOOL hook_isEqual(id self, SEL _cmd, id other) {
-    if ([self isKindOfClass:[NSString class]] && [other isKindOfClass:[NSString class]]) {
-        NSString *s = (NSString *)self;
-        NSString *o = (NSString *)other;
-        if ([s hasPrefix:targetPrefix] || [o hasPrefix:targetPrefix]) {
-            NSString *trace = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
-            NSString *log = [NSString stringWithFormat:@"isEqual:\nself=%@\nother=%@\n\nStack:\n%@", s, o, trace];
+static void hook_setValue(id self, SEL _cmd, NSString *value, NSString *field) {
+    @try {
+        if (value && [value hasPrefix:targetValue]) {
+            NSArray *stack = [NSThread callStackSymbols];
+            NSString *log = [NSString stringWithFormat:@"field=%@\nvalue=%@\n%@",
+                field, value,
+                [stack componentsJoinedByString:@"\n"]
+            ];
             [foundLogs addObject:log];
         }
-    }
-    return orig_isEqual(self, _cmd, other);
-}
-
-// NSString stringWithFormat hook - bu değerin nerede üretildiğini bul  
-typedef NSString* (*stringWithFormat_t)(id, SEL, NSString*, ...);
-
-// NSMutableURLRequest setValue:forHTTPHeaderField: hook - header set edilirken yakala
-static void hook_setValueForHTTPHeaderField(id self, SEL _cmd, NSString *value, NSString *field) {
-    if ([field isEqualToString:@"deviceId"] || [value hasPrefix:targetPrefix]) {
-        NSString *trace = [[NSThread callStackSymbols] componentsJoinedByString:@"\n"];
-        NSString *log = [NSString stringWithFormat:@"[HEADER SET]\nfield=%@\nvalue=%@\n\nStack:\n%@", field, value, trace];
-        [foundLogs addObject:log];
-    }
+    } @catch (NSException *e) {}
     
-    // orijinali çağır
-    IMP orig = class_getMethodImplementation(
-        objc_getClass("NSMutableURLRequest"),
-        @selector(setValue:forHTTPHeaderField:)
-    );
-    ((void(*)(id,SEL,NSString*,NSString*))orig)(self, _cmd, value, field);
+    orig_setValue(self, _cmd, value, field);
 }
 
 @interface DeviceTracer : NSObject
@@ -51,16 +30,18 @@ static void hook_setValueForHTTPHeaderField(id self, SEL _cmd, NSString *value, 
 + (void)load {
     foundLogs = [NSMutableArray array];
     
-    // NSMutableURLRequest hook - deviceId header'ı set edilirken yakala
     Method m = class_getInstanceMethod(
         objc_getClass("NSMutableURLRequest"),
         @selector(setValue:forHTTPHeaderField:)
     );
-    if (m) method_setImplementation(m, (IMP)hook_setValueForHTTPHeaderField);
     
-    // 5 saniye bekle - uygulama istek atsın
+    if (m) {
+        orig_setValue = (void(*)(id,SEL,NSString*,NSString*))method_getImplementation(m);
+        method_setImplementation(m, (IMP)hook_setValue);
+    }
+    
     dispatch_async(dispatch_get_main_queue(), ^{
-        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(5.0 * NSEC_PER_SEC)),
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(6.0 * NSEC_PER_SEC)),
                        dispatch_get_main_queue(), ^{
             
             UIWindow *window = nil;
@@ -74,14 +55,13 @@ static void hook_setValueForHTTPHeaderField(id self, SEL _cmd, NSString *value, 
             
             NSString *logs = foundLogs.count > 0
                 ? [foundLogs componentsJoinedByString:@"\n\n---\n\n"]
-                : @"Henüz tespit edilmedi - daha uzun bekle";
+                : @"Tespit edilemedi";
             
-            // Alert çok uzun olabilir - UIPasteboard'a kopyala
             [UIPasteboard generalPasteboard].string = logs;
             
             UIAlertController *alert = [UIAlertController
-                alertControllerWithTitle:@"Device ID Tracer"
-                message:[NSString stringWithFormat:@"%lu log bulundu.\nPanoya kopyalandı.", (unsigned long)foundLogs.count]
+                alertControllerWithTitle:@"Tracer"
+                message:[NSString stringWithFormat:@"%lu log - panoya kopyalandı", (unsigned long)foundLogs.count]
                 preferredStyle:UIAlertControllerStyleAlert
             ];
             [alert addAction:[UIAlertAction actionWithTitle:@"Tamam" style:UIAlertActionStyleDefault handler:nil]];
